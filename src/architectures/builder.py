1e5bcd6db67a9db9324457dab791b28d54ad0434
Lennard Korte <mail@lennardkorte.com>
2024-02-10 05:56:27 +0800

    update models

--- src/architectures/builder.py
+++ src/architectures/builder.py
@@ -1,6 +1,6 @@
-from . import vgg, resnet, densenet
+from . import vgg, resnet, densenet, inception
 
-def architecture_builder(arch='resnet50', version=2.0, dropout=0.2, num_classes=2):
+def architecture_builder(arch='resnet50', version=2.0, dropout=0.2, num_classes=2, mirror=False, freeze_enc=False, depth=5):
 
     if arch in ["vgg11", "vgg13", "vgg16", "vgg19"]:
         layer_cfg = vgg.get_configs(arch)
@@ -10,21 +10,27 @@ def architecture_builder(arch='resnet50', version=2.0, dropout=0.2, num_classes=
         layer_cfg, bottleneck = resnet.get_configs(arch)
         model = resnet.ResNet(layer_cfg, bottleneck, version, num_classes, dropout)
 
-    elif arch in ["vgg11autenc", "vgg13autenc", "vgg16autenc", "vgg19autenc"]:
-        layer_cfg = vgg.get_configs(arch)
-        model = vgg.VGGAutoEncoder(layer_cfg)
-
-    elif arch in ["resnet18autenc", "resnet34autenc", "resnet50autenc", "resnet101autenc", "resnet152autenc"]:
-        layer_cfg, bottleneck = resnet.get_configs(arch)
-        model = resnet.ResNetAutoEncoder(layer_cfg, bottleneck, version)
-    
     elif arch in ["densenet121","densenet169","densenet201","densenet264"]:
         layer_cfg = densenet.get_configs(arch)
         model = densenet.DenseNet(layer_cfg, num_classes, dropout)
 
+    elif arch in ["inceptionv3"]:
+        model = inception.Inception3(num_classes, dropout)
+
+    elif arch in ["vgg11autenc", "vgg13autenc", "vgg16autenc", "vgg19autenc"]:
+        layer_cfg = vgg.get_configs(arch)
+        model = vgg.VGGAutoEncoder(layer_cfg, mirror, freeze_enc, depth)
+
+    elif arch in ["resnet18autenc", "resnet34autenc", "resnet50autenc", "resnet101autenc", "resnet152autenc"]:
+        layer_cfg, bottleneck = resnet.get_configs(arch)
+        model = resnet.ResNetAutoEncoder(layer_cfg, bottleneck, version, mirror, freeze_enc, depth)
+    
     elif arch in ["densenet121autenc","densenet169autenc","densenet201autenc","densenet264autenc"]:
         layer_cfg = densenet.get_configs(arch)
-        model = densenet.DenseNetAutoEncoder(layer_cfg)
+        model = densenet.DenseNetAutoEncoder(layer_cfg, mirror, freeze_enc, depth)
+
+    elif arch in ["inceptionv3autenc"]:
+        model = inception.Inception3AutoEncoder(mirror, freeze_enc, depth)
 
     else:
         raise ValueError("Undefined model")
--- src/architectures/vgg.py
+++ src/architectures/vgg.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
+import resnet
 
-def get_configs(arch='vgg16'):
+def get_configs(arch):
 
     if arch == 'vgg11':
         layer_cfg = [1, 1, 2, 2, 2]
@@ -18,13 +19,21 @@ def get_configs(arch='vgg16'):
 
 class VGGAutoEncoder(nn.Module):
 
-    def __init__(self, layer_cfg):
+    def __init__(self, layer_cfg, mirror, freeze_enc, depth):
 
         super(VGGAutoEncoder, self).__init__()
 
         # VGG without Bn as AutoEncoder is hard to train
-        self.encoder = VGGEncoder(layer_cfg=layer_cfg,       enable_bn=True)
-        self.decoder = VGGDecoder(layer_cfg=layer_cfg[::-1], enable_bn=True)
+        self.encoder = VGGEncoder(layer_cfg=layer_cfg, depth=depth, enable_bn=True)
+        if freeze_enc:
+            for param in self.encoder.parameters():
+                param.requires_grad = False
+
+        if mirror:
+            self.decoder = VGGDecoder(layer_cfg=layer_cfg[::-1], depth=depth, enable_bn=True)
+        else:
+            layer_cfg, bottleneck = resnet.get_configs("resnet50")
+            self.decoder = resnet.ResNetDecoder(layer_cfg=layer_cfg[::-1], bottleneck=bottleneck, conv_trans=False, depth=depth, in_channels=[512, 512, 256, 128, 64])
         
     
     def forward(self, x):
@@ -39,28 +48,33 @@ class VGG(nn.Module):
     def __init__(self, layer_cfg, num_classes, dropout, img_size, enable_bn=False):
         super(VGG, self).__init__()
 
-        self.encoder = VGGEncoder(layer_cfg=layer_cfg, enable_bn=enable_bn)
+        self.encoder = VGGEncoder(layer_cfg=layer_cfg, depth=5, enable_bn=enable_bn)
 
         self.img_size = img_size / 32
 
         self.fc = nn.Sequential(
             nn.Linear(in_features=int(self.img_size*self.img_size*512), out_features=4096),
-            nn.Dropout(p=0.5),
+            nn.Dropout(p=dropout),
             nn.ReLU(inplace=True),
             nn.Linear(in_features=4096, out_features=4096),
-            nn.Dropout(p=0.5),
             nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
             nn.Linear(in_features=4096, out_features=num_classes)
         )
-        
+
+        # init like in torchvision
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.normal_(m.weight, mean=0, std=0.01)
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
-            if isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, mean=0, std=0.01)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
     
     def forward(self, x):
 
@@ -74,7 +88,8 @@ class VGG(nn.Module):
 
 class VGGEncoder(nn.Module):
 
-    def __init__(self, layer_cfg, enable_bn=False):
+    def __init__(self, layer_cfg, depth, enable_bn=True):
+        self.depth = depth
 
         super(VGGEncoder, self).__init__()
 
@@ -83,44 +98,62 @@ class VGGEncoder(nn.Module):
             raise ValueError("There should be 5 stage in VGG")
 
         self.conv1 = EncoderBlock(input_dim=3,   output_dim=64,  hidden_dim=64,  layers=layer_cfg[0], enable_bn=enable_bn)
-        self.conv2 = EncoderBlock(input_dim=64,  output_dim=128, hidden_dim=128, layers=layer_cfg[1], enable_bn=enable_bn)
-        self.conv3 = EncoderBlock(input_dim=128, output_dim=256, hidden_dim=256, layers=layer_cfg[2], enable_bn=enable_bn)
-        self.conv4 = EncoderBlock(input_dim=256, output_dim=512, hidden_dim=512, layers=layer_cfg[3], enable_bn=enable_bn)
-        self.conv5 = EncoderBlock(input_dim=512, output_dim=512, hidden_dim=512, layers=layer_cfg[4], enable_bn=enable_bn)
+        if self.depth >= 2:
+            self.conv2 = EncoderBlock(input_dim=64,  output_dim=128, hidden_dim=128, layers=layer_cfg[1], enable_bn=enable_bn)
+        if self.depth >= 3:
+            self.conv3 = EncoderBlock(input_dim=128, output_dim=256, hidden_dim=256, layers=layer_cfg[2], enable_bn=enable_bn)
+        if self.depth >= 4:
+            self.conv4 = EncoderBlock(input_dim=256, output_dim=512, hidden_dim=512, layers=layer_cfg[3], enable_bn=enable_bn)
+        if self.depth >= 5:
+            self.conv5 = EncoderBlock(input_dim=512, output_dim=512, hidden_dim=512, layers=layer_cfg[4], enable_bn=enable_bn)
     
     def forward(self, x):
 
         x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
+        if self.depth >= 2:
+            x = self.conv2(x)
+        if self.depth >= 3:
+            x = self.conv3(x)
+        if self.depth >= 4:
+            x = self.conv4(x)
+        if self.depth >= 5:
+            x = self.conv5(x)
 
         return x
 
 class VGGDecoder(nn.Module):
 
-    def __init__(self, layer_cfg, enable_bn=False):
-
+    def __init__(self, layer_cfg, depth, enable_bn=True):
         super(VGGDecoder, self).__init__()
+        self.depth = depth
 
         if len(layer_cfg) != 5:
 
             raise ValueError("There should be 5 stage in VGG")
 
-        self.conv1 = DecoderBlock(input_dim=512, output_dim=512, hidden_dim=512, layers=layer_cfg[0], enable_bn=enable_bn)
-        self.conv2 = DecoderBlock(input_dim=512, output_dim=256, hidden_dim=512, layers=layer_cfg[1], enable_bn=enable_bn)
-        self.conv3 = DecoderBlock(input_dim=256, output_dim=128, hidden_dim=256, layers=layer_cfg[2], enable_bn=enable_bn)
-        self.conv4 = DecoderBlock(input_dim=128, output_dim=64,  hidden_dim=128, layers=layer_cfg[3], enable_bn=enable_bn)
+        if self.depth >= 5:
+            self.conv1 = DecoderBlock(input_dim=512, output_dim=512, hidden_dim=512, layers=layer_cfg[0], enable_bn=enable_bn)
+        if self.depth >= 4:
+            self.conv2 = DecoderBlock(input_dim=512, output_dim=256, hidden_dim=512, layers=layer_cfg[1], enable_bn=enable_bn)
+        if self.depth >= 3:
+            self.conv3 = DecoderBlock(input_dim=256, output_dim=128, hidden_dim=256, layers=layer_cfg[2], enable_bn=enable_bn)
+        if self.depth >= 2:
+            self.conv4 = DecoderBlock(input_dim=128, output_dim=64,  hidden_dim=128, layers=layer_cfg[3], enable_bn=enable_bn)
+        
         self.conv5 = DecoderBlock(input_dim=64,  output_dim=3,   hidden_dim=64,  layers=layer_cfg[4], enable_bn=enable_bn)
         self.gate = nn.Sigmoid()
     
     def forward(self, x):
 
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
+        if self.depth >= 5:
+            x = self.conv1(x)
+        if self.depth >= 4:
+            x = self.conv2(x)
+        if self.depth >= 3:
+            x = self.conv3(x)
+        if self.depth >= 2:
+            x = self.conv4(x)
+
         x = self.conv5(x)
         x = self.gate(x)
 
@@ -245,14 +278,26 @@ class DecoderLayer(nn.Module):
 if __name__ == "__main__":
 
     input = torch.randn((5,3,224,224))
+    print(input.shape)
 
-    layer_cfg = get_configs()
+    layer_cfg = get_configs('vgg19')
 
-    model = VGGAutoEncoder(layer_cfg)
-
-    output = model(input)
-
+    encoder = VGGEncoder(layer_cfg, depth=5)
+    output = encoder(input)
     print(output.shape)
+
+    decoder = VGGDecoder(layer_cfg, depth=5)
+    output = decoder(output)
+    print(output.shape)
+
+    autenc = VGGAutoEncoder(layer_cfg=layer_cfg, mirror=False, freeze_enc=True, depth=5)
+    output = autenc(input)
+    print(output.shape)
+
+    #from torchsummary import summary
+    #summary(encoder)
+    #summary(decoder)
+    #summary(autenc)
 
 
 
