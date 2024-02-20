665e7391a2f5584f5d1bfe6a6aa66304af8193ae
Lennard Korte <mail@lennardkorte.com>
2024-02-21 03:55:38 +0800

    rm old configs

--- runs/classifier_03 copy.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-entrance_script="./exec_docker.sh"
-WANDB_API_KEY="8bea2fc48ca4a501eaec31dbfb410413a640b839"
-
-bash $entrance_script -cfg ./runs/classifier_03.json -wb $WANDB_API_KEY
--- runs/classifier_resnet50v2.sh
+++ runs/classifier_resnet50v2.sh
@@ -3,5 +3,4 @@
 entrance_script="./exec_docker.sh"
 WANDB_API_KEY="8bea2fc48ca4a501eaec31dbfb410413a640b839"
 
-bash $entrance_script -cfg ./runs/decoder_resnet18_9.json -wb $WANDB_API_KEY
-
+bash $entrance_script -cfg ./runs/classifier_resnet50v2.json -wb $WANDB_API_KEY
